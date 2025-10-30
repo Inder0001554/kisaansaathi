5bc33324e9055f5f50d21335807b49348c54e938
inder <ij653588@gmail.com>
2025-10-30 23:02:06 +0530

    Fix import case for User model in authController

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import User from "../models/User.js";
+import User from "../models/user.js";
 
 // 📝 Register User
 export const registerUser = async (req, res) => {
